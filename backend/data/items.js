877264547c982886b22334d0e43511d8d92f63de
abhikr-01 <abhi19o7kr@gmail.com>
2026-04-15 15:30:13 +0530

    Myntra Clone using React with backend Server

---
+++ backend/data/items.js
@@ -0,0 +1,18 @@
+const fs = require("node:fs/promises");
+const path = require("path");
+
+async function getStoredItems() {
+  const rawFileContent = await fs.readFile(path.join(__dirname, "../items.json"), {
+    encoding: "utf-8",
+  });
+  const data = JSON.parse(rawFileContent);
+  const storedItems = data.items ?? [];
+  return storedItems;
+}
+
+function storeItems(items) {
+  return fs.writeFile("../items.json", JSON.stringify({ items: items || [] }));
+}
+
+exports.getStoredItems = getStoredItems;
+exports.storeItems = storeItems;
